5e8076e4ced746b679b0c94dfb019efdb04b2fc3
Ankit Kr. Singh <ankitsingh095@outlook.com>
2026-07-31 17:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (16.87%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -8,18 +8,16 @@ class Solution:
 
         while(j<n):
             if(nums[i] == nums[j]):
-                # duplicate = True
                 duplicateExisted = True
                 j+=1
                 continue
             else:
                 if(j-i > 1):
                     self.swap(nums, i+1, j)
-                    # duplicate = False
                 i+=1
             j+=1
 
-        return i+1 if duplicateExisted else n
+        return i+1
     
     def swap(self, arr, i, j):
         arr[i], arr[j] = arr[j], arr[i]
