238239c2301549c53f136eac7c5f7560cc04099f
Ankit Kr. Singh <ankitsingh095@outlook.com>
2026-08-01 00:32:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (60.76%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -7,21 +7,25 @@ class Solution:
         i = 0
         j = 0
         n = len(nums)
+        k = n-1
 
-        while(j<n):
+        while(j<=k):
             if (nums[j] == 0):
                 nums[i], nums[j] = nums[j], nums[i]
                 i+=1
                 j+=1
-            else:
+            elif nums[j] == 1:
                 j+=1
+            else:
+                nums[k], nums[j] = nums[j], nums[k]
+                k-=1
 
         # i = 0
-        j = i
-        while(j<n):
-            if (nums[j] == 1):
-                nums[i], nums[j] = nums[j], nums[i]
-                i+=1
-                j+=1
-            else:
-                j+=1
+        # j = i
+        # while(j<n):
+        #     if (nums[j] == 1):
+        #         nums[i], nums[j] = nums[j], nums[i]
+        #         i+=1
+        #         j+=1
+        #     else:
+        #         j+=1
