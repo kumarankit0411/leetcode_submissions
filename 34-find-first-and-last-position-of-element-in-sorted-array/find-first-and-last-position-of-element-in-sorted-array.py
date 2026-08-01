85da994a38b9f78fb975381fd3cfd380201df0cf
Ankit Kr. Singh <ankitsingh095@outlook.com>
2026-08-01 16:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (59.24%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -22,21 +22,3 @@ class Solution:
                 r = m - 1
         
         return ans
-
-    # def lo(self, nums, target):
-    #     l = 0
-    #     r = len(nums) - 1
-    #     ans = -1
-
-    #     while l<=r:
-    #         m = l + (r-l)//2
-
-    #         if nums[m] == target:
-    #             ans = m
-    #             l = m + 1
-    #         elif nums[m] < target:
-    #             l = m + 1
-    #         else:
-    #             r = m - 1
-        
-    #     return ans
