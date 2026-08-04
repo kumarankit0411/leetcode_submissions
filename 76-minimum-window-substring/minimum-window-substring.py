d4af6850bf765b1e905ffce69b0edd897400bfcb
Ankit Kr. Singh <ankitsingh095@outlook.com>
2026-08-05 01:26:01 +0530

    Time: 127 ms (28.78%) | Memory: 19.9 MB (11.41%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,37 @@
+from collections import Counter
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not(s) or not(t) or len(s)<len(t):
+            return ''
+
+        t_dict = Counter(t)
+        window_dict = {}
+
+        have = 0
+        need = len(t_dict)
+
+        res = ""
+        res_len = float('inf')
+
+        i = 0
+        for j in range(len(s)):
+            char = s[j]
+            window_dict[char] = window_dict.get(char, 0) + 1
+
+            if char in t_dict and window_dict[char] == t_dict[char]:
+                have += 1
+                
+            while (have == need):
+                if (j-i+1) < res_len:
+                    res_len = j - i + 1
+                    res = s[i:j+1]
+
+                left_char = s[i]
+                window_dict[left_char] -=1
+
+                if left_char in t_dict and window_dict[left_char] < t_dict[left_char]:
+                    have -= 1
+
+                i+=1
+
+        return res
