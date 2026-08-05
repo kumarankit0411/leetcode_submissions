30789a65e08d629a5688730b0c8b79e0b723eb6e
Ankit Kr. Singh <ankitsingh095@outlook.com>
2026-08-05 23:14:41 +0530

    Time: 84 ms (53.22%) | Memory: 19.8 MB (15.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,49 +1,22 @@
-from collections import Counter
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         i=0
         j=0
+        max_freq = 0
         s_dict = [0] * 26
-        s_sum = 0
         longest = -float('inf')
 
-        # for m in range(k+1):
-        #     s_dict[ord(s[m])%26] += 1
-
-        while j < len(s):
+        for j in range(len(s)):
             s_dict[ord(s[j])%26] += 1
 
-            s_sum = self.findSumExcludingHighest(s_dict)
-            print(s_sum)
+            max_freq = max(max_freq, s_dict[ord(s[j])%26])
 
-            while s_sum > k and i<len(s) - 1:
-                # valid
+            while (j - i + 1) - max_freq > k:
                 s_dict[ord(s[i])%26] -= 1
                 i+=1
-                s_sum = self.findSumExcludingHighest(s_dict)
 
             if j - i + 1 > longest:
                 longest = j - i + 1
-
-            j+=1
         
         return longest
-            
-    def findSumExcludingHighest(self, arr):
-        high = 0
-        index = -1
-        for i in range(len(arr)):
-            if arr[i] > high:
-                high = arr[i]
-                index = i
-
-        res = 0
-        for i in range(len(arr)):
-            if i == index or arr[i] == 0:
-                continue
-            else:
-                res += arr[i]
-
-        return res
-
 
