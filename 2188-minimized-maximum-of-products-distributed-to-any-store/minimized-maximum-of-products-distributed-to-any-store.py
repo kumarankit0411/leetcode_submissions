a32e42906ec93f4e6fdbf0cad086b6366c64513b
Ankit Kumar Singh <ankitsingh095@outlook.com>
2026-08-12 23:02:06 +0530

    Time: 587 ms (95.83%) | Memory: 33.2 MB (73.37%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.py
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.py
@@ -1,14 +1,12 @@
 class Solution:
     def minimizedMaximum(self, n: int, arr: List[int]) -> int:
         # return ceil(sum(arr)/n) # this solves more than half test cases
-        l = 0
+        l = 1
         r = max(arr)
         minX = r
 
         while l<=r:
             m=(l+r)//2
-            if m==0:
-                return 1
             if self.isPossible(arr, m, n):
                 r = m - 1
                 if minX > m:
