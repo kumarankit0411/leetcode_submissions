0c81560354d1f1832adef1845891fb1f5e8ef519
Ankit Kumar Singh <ankitsingh095@outlook.com>
2026-08-21 14:32:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (52.81%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -1,39 +1,25 @@
+from collections import deque
 class MyStack:
 
     def __init__(self):
-        self.q1 = list()
-        self.q2 = list()
+        self.q1 = deque()
+        # self.q2 = list()
 
     def push(self, x: int) -> None:
         self.q1.append(x)
-        if (len(self.q2) != 0):
-            self.q2.pop(0)
-        self.q2.append(x)  
+
+        #rotate
+        for _ in range(len(self.q1) - 1):
+            self.q1.append(self.q1.popleft())
 
     def pop(self) -> int:
-        val = self.q2.pop(0)
-        second_last = None
-        while len(self.q1) > 0:
-            top = self.q1.pop(0)
-            if len(self.q1) == 1:
-                second_last = top
-            if len(self.q1) != 0:
-                self.q2.append(top)
-        
-        if second_last is not None:
-            self.q1.append(second_last)
-
-        temp = self.q1
-        self.q1 = self.q2
-        self.q2 = temp
-
-        return val
+        return self.q1.popleft()
 
     def top(self) -> int:
-        return self.q2[0]
+        return self.q1[0]
 
     def empty(self) -> bool:
-        return len(self.q2)==0
+        return len(self.q1)==0
 
 
 # Your MyStack object will be instantiated and called as such:
