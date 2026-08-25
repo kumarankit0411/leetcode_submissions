60e8550bb27e68937e4a12f8c67f968c6014106c
Ankit Kumar Singh <ankitsingh095@outlook.com>
2026-08-25 22:37:40 +0530

    Time: 31 ms (74.21%) | Memory: 20.7 MB (49.26%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -11,20 +11,15 @@ class Solution:
                 else:
                     stack.append((s[i], i))
         
-        print(stack)
         if len(stack) == 0:
             return s
 
         s_list = list(s)
         
         for _, idx in stack:
-            # s = s[:idx] + '_' + s[idx + 1:]
             s_list[idx] = ''
 
         s = ''.join(s_list)
 
-        # s = s.replace('_', '') 
-
-        print(s)
         return(s)
             
