27b71591166e88bd3bc314c7758cc0000907319e
Ankit Kumar Singh <ankitsingh095@outlook.com>
2026-09-08 00:39:45 +0530

    Time: 287 ms (5.07%) | Memory: 34.8 MB (29.71%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -0,0 +1,51 @@
+class Solution:
+    def find_nsor_span(self, arr):
+        res = [-1] * len(arr)
+        stack = []
+        n = len(arr)
+
+        for i in range(len(arr) - 1, -1 , -1):
+            x = arr[i]
+
+            while stack and stack[-1][0] >= x:
+                stack.pop()
+
+            res[i] = stack[-1][1] - 1 if stack else n - 1
+
+            stack.append((x, i))
+
+        return res
+
+    def find_nsol_span(self, arr):
+        res = [-1] * len(arr)
+        stack = []
+
+        for i in range(len(arr)):
+            x = arr[i]
+
+            while stack and stack[-1][0] >= x:
+                stack.pop()
+
+            res[i] = stack[-1][1] + 1 if stack else 0
+
+            stack.append((x, i))
+
+        return res
+
+    def largestRectangleArea(self, arr: List[int]) -> int:
+        nsor = self.find_nsor_span(arr)
+        nsol = self.find_nsol_span(arr)
+
+        max_area = 0
+
+        for i in range(len(arr)):
+            area = arr[i] * ((nsor[i] - i + 1) + (i - nsol[i] + 1)) - arr[i]
+
+            if area > max_area:
+                max_area = area
+
+
+        print(nsor)
+        print(nsol)
+        print(max_area)
+        return max_area
